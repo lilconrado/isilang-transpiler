f5102ea2303162e0f39773fde97b4b7ebe6dd0b9
unknown <lucas.serna.quinto@gmail.com>
2023-08-19 21:59:45 -0300

    adicionando scanner unico

--- src/io/github/lilconrado/isilang/ast/CmdRead.java
+++ src/io/github/lilconrado/isilang/ast/CmdRead.java
@@ -3,6 +3,7 @@ package io.github.lilconrado.isilang.ast;
 import io.github.lilconrado.isilang.symbols.Identifier;
 
 public class CmdRead extends AbstractCommand {
+    static boolean firstScanner = false;
     private Identifier id;
     public CmdRead(Identifier id) {
         super();
@@ -10,17 +11,20 @@ public class CmdRead extends AbstractCommand {
     }
     @Override
     public String generateCode() {
-        // scanner precisa trazer uma vez soh
-        String code = "";
+        String code = "" ;
+        if(!firstScanner){
+            code= "Scanner scanner = new Scanner(System.in);\n";
+            firstScanner = true;
+        }
         switch(id.getType()){
             case INTEGER:
-                code = String.format("Scanner scanner = new Scanner(System.in);\n%s=.nextInt()", id.getName());
+                code += String.format("%s=.nextInt();\n", id.getName());
                 break;
             case REAL:
-                code = String.format("Scanner scanner = new Scanner(System.in);\n%s=.nextDouble()", id.getName());
+                code += String.format("%s=.nextDouble();\n", id.getName());
                 break;
             case STRING:
-                code = String.format("Scanner scanner = new Scanner(System.in);\n%s=.nextLine()", id.getName());
+                code += String.format("%s=.nextLine();\n", id.getName());
                 break;
             default:
                 throw new RuntimeException();
