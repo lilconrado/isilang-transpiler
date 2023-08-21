9582822a0035914bcb3eecd562d66c1b7658c8bd
unknown <lucas.serna.quinto@gmail.com>
2023-08-20 21:13:18 -0300

    adicionando setLanguage

--- src/io/github/lilconrado/isilang/main/Main.java
+++ src/io/github/lilconrado/isilang/main/Main.java
@@ -9,14 +9,14 @@ import org.antlr.v4.runtime.CommonTokenStream;
 public class Main {
     public static void main(String[] args) {
         try {
-            String languageType = args[0].isEmpty() ? "java" : args[0];
+            String languageType = args.length == 0 ? "java" : args[0];
             var charStream = CharStreams.fromFileName("input.isi");
 
             IsiLangLexer lexer = new IsiLangLexer(charStream);
             var tokenStream = new CommonTokenStream(lexer);
 
             var parser = new IsiLangParser(tokenStream);
-
+            parser.setLanguage(languageType);
             parser.init();
             var a = parser.prog();
             parser.verifyUnused();
